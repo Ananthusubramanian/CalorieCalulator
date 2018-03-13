ff68930ffefc2d929e291ee904f23d05382b2274
Ananthu <anandusubramaian@gmail.com>
2018-03-13 23:53:29 +0530

    Added all data's

--- app/src/main/java/com/blospot/techscreator/caloriecalculator/Enterdata.java
+++ app/src/main/java/com/blospot/techscreator/caloriecalculator/Enterdata.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 
 
 public class Enterdata extends AppCompatActivity {
-    private TextInputEditText name,age,gender,weight,hours;
+    private TextInputEditText name,age,gender,weight,hours,heartrate;
     private Button sub;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +31,8 @@ public class Enterdata extends AppCompatActivity {
         weight=(TextInputEditText)findViewById(R.id.weight);
         hours=(TextInputEditText)findViewById(R.id.hours);
         sub=(Button)findViewById(R.id.b_submit);
+        heartrate= findViewById(R.id.heartrate);
+
         sub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,7 +46,8 @@ public class Enterdata extends AppCompatActivity {
                     int hour=Integer.parseInt(hours.getText().toString());
                     int ag =Integer.parseInt(gender.getText().toString());
                     int wt= Integer.parseInt(weight.getText().toString());
-                    double cal=((0.02017*ag)+(0.6309*hour)-(0.09036*wt)-55.0969)*(hour/4.184);
+                    int rate = Integer.parseInt(heartrate.getText().toString());
+                    double cal=(((0.02017*ag)-(rate*0.05741)+(ag*0.4472)-20.4022)*(hour*60))/4.184;
                     if(hour>0&&ag>0&&wt>0&&!name.getText().toString().isEmpty()&&!gender.getText().toString().isEmpty()) {
                         User u = new User(t, name.getText().toString(), age.getText().toString().trim(), gender.getText().toString(), weight.toString(), hours.toString(), cal);
                         db.add(u);
